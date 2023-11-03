52079d383da661d2629166dea83e9f3668f7ce62
Luis4nge1 <luis24.lmx@gmail.com>
2023-11-03 00:14:04 -0500

    Acoplando nueva ventana

---
+++ Avance_Proyecto/app/src/main/java/com/example/avance_proyecto/data/UsuarioDataSource.kt
@@ -0,0 +1,83 @@
+package com.example.avance_proyecto.data
+
+import com.example.avance_proyecto.R
+import com.example.avance_proyecto.data.uistate.UsuarioUiState
+
+object UsuarioDataSource {
+
+    val usuarios = listOf(
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto1,
+            puesto = R.string.puesto1,
+            predio = R.string.predio1,
+            ubicacion = R.string.ubicacion1,
+            tiempo = R.string.tiempo1/*,
+            imageRes = R.drawable.android_superhero1*/
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto2,
+            puesto = R.string.puesto2,
+            predio = R.string.predio2,
+            ubicacion = R.string.ubicacion2,
+            tiempo = R.string.tiempo2/*,
+            imageRes = R.drawable.android_superhero2*/
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto3,
+            puesto = R.string.puesto3,
+            predio = R.string.predio3,
+            ubicacion = R.string.ubicacion3,
+            tiempo = R.string.tiempo3
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto4,
+            puesto = R.string.puesto4,
+            predio = R.string.predio4,
+            ubicacion = R.string.ubicacion4,
+            tiempo = R.string.tiempo4
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto5,
+            puesto = R.string.puesto5,
+            predio = R.string.predio5,
+            ubicacion = R.string.ubicacion5,
+            tiempo = R.string.tiempo5
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto6,
+            puesto = R.string.puesto6,
+            predio = R.string.predio6,
+            ubicacion = R.string.ubicacion6,
+            tiempo = R.string.tiempo6
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto6,
+            puesto = R.string.puesto6,
+            predio = R.string.predio6,
+            ubicacion = R.string.ubicacion6,
+            tiempo = R.string.tiempo6
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto6,
+            puesto = R.string.puesto6,
+            predio = R.string.predio6,
+            ubicacion = R.string.ubicacion6,
+            tiempo = R.string.tiempo6
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto6,
+            puesto = R.string.puesto6,
+            predio = R.string.predio6,
+            ubicacion = R.string.ubicacion6,
+            tiempo = R.string.tiempo6
+        ),
+        UsuarioUiState(
+            nombrecompleto = R.string.nombrecompleto6,
+            puesto = R.string.puesto6,
+            predio = R.string.predio6,
+            ubicacion = R.string.ubicacion6,
+            tiempo = R.string.tiempo6
+        )
+    )
+
+}
---
+++ Avance_Proyecto/app/src/main/java/com/example/avance_proyecto/data/uistate/UsuarioUiState.kt
@@ -0,0 +1,20 @@
+package com.example.avance_proyecto.data.uistate
+
+import androidx.annotation.StringRes
+
+data class UsuarioUiState (
+
+    @StringRes
+    val nombrecompleto: Int,
+    @StringRes
+    val puesto: Int,
+    @StringRes
+    val predio: Int,
+    @StringRes
+    val ubicacion: Int,
+    @StringRes
+    val tiempo: Int,
+    /*,
+    @DrawableRes val imageRes: Int*/
+
+)
